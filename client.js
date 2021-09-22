d30f55c9beff0c033269a64a95a828640497c41f
Mohammed Ali Al-Hassan <ali.alhassan30@gmail.com>
2021-09-22 19:52:16 +0000

    Added name and sending it to the server

--- client.js
+++ client.js
@@ -1,16 +1,22 @@
 const net = require("net");
+const { stdin } = require("process");
 
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
     host: '135.23.223.133',
     port: 50542
-
-
+  }, () => {
+    console.log("Connection successful")
   });
 
+  conn.on
   // interpret incoming data as text
   conn.setEncoding("utf8");
+  stdin.setEncoding("utf8");
+
+  conn.write(`Name: ghi has connected!!!`);
+
 
   conn.on('data', (data) => {
      console.log(data);
