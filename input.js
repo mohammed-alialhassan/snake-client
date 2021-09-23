2ff6083deb745d10d907ca52f5f242f0d899f7dd
Mohammed Ali Al-Hassan <ali.alhassan30@gmail.com>
2021-09-23 02:45:21 +0000

    WASD movements added

---
+++ input.js
@@ -0,0 +1,33 @@
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+};
+
+const handleUserInput = key => {
+  if (key === "\u0003") {
+    process.exit();
+  }
+  if (key === "w") {
+    connection.write("Move: up");
+  }
+  if (key === "a") {
+    connection.write("Move: left");
+  }
+  if (key === "s") {
+    connection.write("Move: down");
+  }
+  if (key === "d") {
+    connection.write("Move: right");
+  }
+  if (key === "e") {
+    connection.write("Say: Haha gotcha!");
+  }
+};
+
+module.exports = setupInput; 
