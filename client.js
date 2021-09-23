e82532020a8bcf6dba37eba71fb2790acc15e832
Mohammed Ali Al-Hassan <ali.alhassan30@gmail.com>
2021-09-23 02:44:55 +0000

    IP and PORT referred to through constants

--- client.js
+++ client.js
@@ -1,25 +1,24 @@
 const net = require("net");
 const { stdin } = require("process");
+const { IP, PORT } = require("./constants");
 
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: '135.23.223.133',
-    port: 50542
+    host: IP,
+    port: PORT
   }, () => {
     console.log("Connection successful")
   });
-
-  conn.on
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  stdin.setEncoding("utf8");
-
-  conn.write(`Name: ghi has connected!!!`);
-
+  conn.setEncoding('utf8'); 
 
   conn.on('data', (data) => {
-     console.log(data);
+    console.log("Server returning: ", data);
+  });
+
+  conn.on('connect', () => {
+    console.log("Successfully connect to game server")
+    conn.write('Name: BUG');
   });
 
   return conn;
